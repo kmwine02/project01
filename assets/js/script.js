9a15c2768c74cd11d4a47e8c8f8d37c389439c88
Zuke27 <wstaylor27@gmail.com>
2021-09-16 22:00:11 -0400

    Updated js and html to fully function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,13 @@ $("#search").on("click", function () {
 });
 
 function searchArtistApi(artist) {
-  var apiUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + apiKey + "&date=" + dateParam;
+  var apiUrl =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=" +
+    apiKey +
+    "&date=" +
+    dateParam;
   console.log(apiUrl);
 
   fetch(apiUrl).then(function (response) {
@@ -43,55 +49,41 @@ function findBrewery(city) {
       return response.json();
     })
     .then(function (data) {
-      console.log("***breweries***", data);
+      // Store breweries
+      var breweryArray = [];
 
       for (i = 0; i < data.length; i++) {
-
-
-        // Store breweries
-        var savedBrewery = localStorage.getItem("brewery");
-        var breweryArray;
-
-        if (savedBrewery === null) {
-          breweryArray = [];
-        } else {
-          breweryArray = JSON.parse(savedBrewery)
-        }
-
         var breweryDetail = {
           name: data[i].name,
           address: data[i].street,
           city: data[i].city,
           phone: data[i].phone,
-          site: data[i].website_url
+          site: data[i].website_url,
         };
 
         console.log(breweryDetail);
         breweryArray.push(breweryDetail);
-
-        // Stringify array in order to store in local
-        var breweryArrayString = JSON.stringify(breweryArray);
-        window.localStorage.setItem("brewery", breweryArrayString);
       }
+      // Stringify array in order to store in local
+      var breweryArrayString = JSON.stringify(breweryArray);
+      window.localStorage.setItem("brewery", breweryArrayString);
 
       window.location.href = "brewery-search.html";
-      getBreweryCard();
     });
-
 }
 
 function buildArtistCard(eventDate, venueName, location, city, i) {
-
   var artistCard = $("<div class='card text-center' id='card" + i + "'></div>");
-  var dateEl = $("<div class='card-header' id='event-date'>" + eventDate + "</div>");
+  var dateEl = $(
+    "<div class='card-header' id='event-date'>" + eventDate + "</div>"
+  );
   var cardBody = $("<div class='card-body'></div>");
   var venueEl = $("<h5 class='card-title' id='venue'>" + venueName + "</h5>");
   var locationEl = $("<p class='card-text' id='location'>" + location + "</p>");
   var buttonEl = $("<button class='btn btn-primary'>Find a brewery</button>");
   buttonEl.on("click", function () {
-    findBrewery(city)
+    findBrewery(city);
   });
-
 
   cardBody.append(venueEl);
   cardBody.append(locationEl);
@@ -99,46 +91,4 @@ function buildArtistCard(eventDate, venueName, location, city, i) {
   artistCard.append(dateEl);
   artistCard.append(cardBody);
   return artistCard;
-
 }
-
-function getBreweryCard() {
-
-  var savedBrewery = localStorage.getItem("brewery");
-
-  // check if there is any in local storage
-  if (savedBrewery === null) {
-    return;
-  }
-  console.log(savedBrewery);
-
-  var storedBrewery = JSON.parse(savedBrewery);
-  for (i = 0; i < storedBrewery.length; i++) {
-    var card = buildBreweryCard();
-    $("#brewery-container").append(card);
-
-
-  }
-}
-
-function buildBreweryCard() {
-  var breweryCard = $("<div class='card text-center' id='card'></div>");
-  var nameEl = $("<div class='card-header' id='event-date'>" + storedBrewery[i].name + "</div>");
-  var cardBody = $("<div class='card-body'></div>");
-  var addressEl = $("<h5 class='card-title' id='venue'>" + storedBrewery[i].address + "</h5>");
-  var cityEl = $("<p class='card-text' id='location'>" + storedBrewery[i].city + "</p>");
-  var phoneEl = $("<p class='card-text' id='location'>" + storedBrewery[i].phone + "</p>");
-  var buttonEl = $("<button class='btn btn-primary'><a href='" + storedBrewery[i].website_url + "' target='_blank'>Visit the brewery's website</a></button>");
-
-
-  cardBody.append(addressEl);
-  cardBody.append(cityEl);
-  cardBody.append(phoneEl);
-  cardBody.append(buttonEl);
-  breweryCard.append(nameEl);
-  breweryCard.append(cardBody);
-  return breweryCard;
-
-}
-
-
