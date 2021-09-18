575925e2fd3f10ec40d3e4ce23924d429be9f4d5
wstaylor27 <54382901+wstaylor27@users.noreply.github.com>
2021-09-18 10:05:29 -0400

    Revert "Wesley branch"

---
+++ assets/js/display-search.js
@@ -0,0 +1,20 @@
+var requestUrl = "https://api.openbrewerydb.org/breweries";
+
+fetch(requestUrl)
+    .then(function (response) {
+        // console.log(response)
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+// var requestUrl2 = 'https://rest.bandsintown.com/artists/pink/events';
+
+// fetch(requestUrl2, {mode:"no-cors"})
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
